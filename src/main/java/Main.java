90b672b8e73ad50a1ff4f14594a14f3b661cb909
Marat Krahin <paparasum@gmail.com>
2020-08-24 15:50:16 +0300

    adapter hw

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        //демонстрация
+        System.out.println(calc.pow(9, 9));
+
+        System.out.println(calc.mult(9, 9));
+
+        System.out.println(calc.sum(9, 9));
+    }
+}
